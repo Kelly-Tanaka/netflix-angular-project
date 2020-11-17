9a3e1f78a78534ed228e40fabba8e6d0f4e623b4
unknown <sah_tanaka@hotmail.com>
2020-11-17 22:20:34 +0000

    init

---
+++ src/app/shared/list-titles/list-titles.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list-titles',
+  templateUrl: './list-titles.component.html',
+  styleUrls: ['./list-titles.component.css']
+})
+export class ListTitlesComponent implements OnInit {
+
+  constructor() { }
+  @Input() title: string;
+
+  ngOnInit(): void {
+  }
+
+}
